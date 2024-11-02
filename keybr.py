3d98fc305c83d5029e430bf72645adc06a8a2e0b
SowmyaKasturi <sowmyakasturisrishailan@gmail.com>
2024-11-02 13:13:04 +0530

    init

---
+++ keybr.py
@@ -0,0 +1,23 @@
+# 1.have a list of words
+# 2.generate random word paragraph
+# 3.move forward only when the correct character is input
+# 4.repeat step 2 and 3 for 10 times
+
+import random
+from string import ascii_lowercase
+
+
+count=0
+while count < 10:
+    llist=random.sample(ascii_lowercase,5)
+    _str = "".join(llist)
+    print(_str)
+    i = 0
+    while True:
+        l = input()
+        if l == _str[i]:
+            i += 1
+            if i == len(_str):
+                break
+
+    count += 1
